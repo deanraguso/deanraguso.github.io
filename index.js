e302d24e497ddadd18e29d0e4363dc07d5d26667
dean <deanraguso@gmail.com>
2021-06-13 21:47:45 +1000

    Created: Side panel that links to my key information.

---
+++ index.js
@@ -0,0 +1,15 @@
+// Adds the navigation link text on side-panel
+document.querySelectorAll("div.contact_link").forEach(element => {
+    element.addEventListener("mouseover", (e) => {
+        let contact_link_text = e.currentTarget.querySelector("p.contact_link_name");
+        setTimeout( function() {
+            contact_link_text.style.display="inline-block";
+        }
+        ,100);
+    });
+
+    element.addEventListener("mouseout", (e) => {
+        let contact_link_text = e.currentTarget.querySelector("p.contact_link_name");
+        contact_link_text.style.display="none";
+    });
+});
