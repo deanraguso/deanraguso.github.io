66f3e12b388b62811252a76697c9a724a2bd75ca
dean <deanraguso@gmail.com>
2021-06-15 08:10:51 +1000

    Created: Wiggle animation, that only works when mouse isn't over the links.

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 // Adds the navigation link text on side-panel
-document.querySelectorAll("div.contact_link").forEach(element => {
+document.querySelectorAll("div.contact_link").forEach((element, index) => {
+
+    let mo = false;
+
     element.addEventListener("mouseover", (e) => {
+        element.style.width = "18vh";
         let contact_link_text = e.currentTarget.querySelector("p.contact_link_name");
         contact_link_text.mouseIsOver = true;
+        this.mo = true;
+
         setTimeout( function() {
             contact_link_text.style.display = 
                 contact_link_text.mouseIsOver ? "inline-block" : "none";
@@ -11,12 +17,30 @@ document.querySelectorAll("div.contact_link").forEach(element => {
     });
 
     element.addEventListener("mouseout", (e) => {
+        element.style.width = "7vh";
         let contact_link_text = e.currentTarget.querySelector("p.contact_link_name");
         contact_link_text.mouseIsOver = false;
+        this.mo = false;
+
         setTimeout( function() {
             contact_link_text.style.display = 
                 contact_link_text.mouseIsOver ? "inline-block" : "none";
         }
         ,0);
     });
+
+    // Adds random wiggles to each link
+    setInterval((e, index) => {
+        setTimeout( function(e) {
+            e.style.width = this.mo ? e.style.width :"12vh";
+        }
+        ,index*200, e);
+
+        setTimeout( function(e) {
+            e.style.width = this.mo ? e.style.width :"7vh";
+        }
+        ,index*200 + 100, e);
+
+    }, 3000, element, index); //Binds to function.
+
 });
