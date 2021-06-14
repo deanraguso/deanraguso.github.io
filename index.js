6e707abe39398dc077ac76077a1c969b8426d15e
dean <deanraguso@gmail.com>
2021-06-14 12:57:09 +1000

    Added: Improvement to callback link display functions, no glitchiness at all.

--- index.js
+++ index.js
@@ -2,17 +2,21 @@
 document.querySelectorAll("div.contact_link").forEach(element => {
     element.addEventListener("mouseover", (e) => {
         let contact_link_text = e.currentTarget.querySelector("p.contact_link_name");
+        contact_link_text.mouseIsOver = true;
         setTimeout( function() {
-            contact_link_text.style.display="inline-block";
+            contact_link_text.style.display = 
+                contact_link_text.mouseIsOver ? "inline-block" : "none";
         }
-        ,70);
+        ,90);
     });
 
     element.addEventListener("mouseout", (e) => {
         let contact_link_text = e.currentTarget.querySelector("p.contact_link_name");
+        contact_link_text.mouseIsOver = false;
         setTimeout( function() {
-            contact_link_text.style.display="none";
+            contact_link_text.style.display = 
+                contact_link_text.mouseIsOver ? "inline-block" : "none";
         }
-        ,70);
+        ,0);
     });
 });
